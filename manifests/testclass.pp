961bfc21428d438d00e98db492b9d5f575d49c1e
Jeffrey Frelick <jeff@autostructure.io>
2017-10-30 15:04:49 -0400

    testclass and testclasify added to simply test classification.

--- manifests/testclass.pp
+++ manifests/testclass.pp
@@ -1,8 +1,9 @@
 class testclass {
-# for debug output on the puppet master
-notice('testclass successful!')
 
-# for debug output on the puppet client
-notify {'testclass successful!':}
+  # for debug output on the puppet master
+  notice('testclass successful!')
+
+  # for debug output on the puppet client
+  notify {'testclass successful!':}
 
 }
