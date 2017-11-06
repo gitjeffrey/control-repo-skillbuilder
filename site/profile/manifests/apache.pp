2769bdeafe20ee973a5b444a76e77dad86bf27df
Jeffrey Frelick <jeff@autostructure.io>
2017-11-06 10:00:54 -0500

    Apache error

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -8,7 +8,9 @@ class profile::apache (
   $vhosts.each | String $key, Hash $value| {
 
     ::apache::vhost { $key:
-      docroot => $value['docroot'],
+      docroot    => $value['docroot'],
+      port       => $value['port'],
+      vhost_name => $value['vhost_name'],
     }
   }
 }
