64325541e3be301e04d2b1e95255893265893254
Jeffrey Frelick <jeff@autostructure.io>
2017-11-28 11:17:11 -0500

    ntp

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,5 +28,12 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  # This is where you can declare classes for all nodes.
+  # Example:
+  #   class { 'my_class': }
+  if $facts['kernel'] == 'Linux' {
+    class { 'ntp':
+      servers => [ 'time.nist.gov' ],
+    }
+  }
 }
-
