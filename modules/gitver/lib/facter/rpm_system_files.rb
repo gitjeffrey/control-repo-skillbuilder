bf142e5430bb5685f2b19d54160e83ef05431cc5
Jeffrey Frelick <jeff@autostructure.io>
2017-12-11 10:32:22 -0500

    ruby facter not working, trying fact in gitver module

---
+++ modules/gitver/lib/facter/rpm_system_files.rb
@@ -0,0 +1,11 @@
+# rpm_system_files.rb
+# V-71849
+# To see a custom fact, run "puppet facts".
+
+Facter.add('rpm_system_files') do
+  setcode do
+    # Facter::Core::Execution.exec("rpm -Va | grep \'^.M\' | grep -o \'/.*$\'")
+    Facter::Core::Execution.exec("rpm -Va | grep \'^.M\'")
+    # .split("\n")
+  end
+end
