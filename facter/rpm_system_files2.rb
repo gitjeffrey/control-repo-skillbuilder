55f8b708bad09e05e8f017b9a4a0f192d95b1844
Jeffrey Frelick <jeff@autostructure.io>
2017-12-11 11:35:35 -0500

    checking which dirs work for custom facts

--- facter/rpm_system_files2.rb
+++ facter/rpm_system_files2.rb
@@ -1,9 +1,11 @@
+# WRONG DIRECTORY
+# try "custom/lib/facter"?
+
 # rpm_system_files.rb
 # V-71849
 # To see a custom fact, run "puppet facts" or "facter -p rpm_system_files"
 
-Facter.add('rpm_system_files') do
-  confine :kernel => 'Linux'
+Facter.add('rpm_system_files2') do
   setcode do
     Facter::Core::Execution.exec("rpm -Va | grep \'^.M\' | grep -o \'/.*$\'").split("\n")
   end
