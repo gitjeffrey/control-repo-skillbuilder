6cb9f32f66c4c1b4376e17cd23928a18ae4530a2
Jeffrey Frelick <jeff@autostructure.io>
2017-12-11 09:51:26 -0500

    ruby string to array

--- lib/facter/rpm_system_files.rb
+++ lib/facter/rpm_system_files.rb
@@ -4,6 +4,9 @@
 
 Facter.add('rpm_system_files') do
   setcode do
-    Facter::Core::Execution.execute("rpm -Va | grep -o \'/.*$\'", {}).split("\n")
+    String return_fact = "empty"
+    return_fact = Facter::Core::Execution.exec("rpm -Va | grep -o \'/.*$\'")
+    Array return_fact_array = return_fact.split("\n")
+    return_fact_array
   end
 end
