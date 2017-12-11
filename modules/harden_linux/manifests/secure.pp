063108c4e5a2bd3975d5567686e295c84c50a84f
Jeffrey Frelick <jeff@autostructure.io>
2017-12-11 14:35:36 -0500

    Working on Vulnerability V-71849

--- modules/harden_linux/manifests/secure.pp
+++ modules/harden_linux/manifests/secure.pp
@@ -2,29 +2,30 @@
 class harden_linux::secure {
 
   # Vulnerability ID | Rule Name:
-
   # V-71849 | SRG-OS-000257-GPOS-00098
-  Array rpm_files = $facts['rpm_system_files']
-  String rpm_package = ''
+  Array harden_linux::rpm_files = $facts['rpm_system_files']
 
-  $rpm_files.each |String $rpm_filename| {
+  $harden_linux::rpm_files.each |String $rpm_filename| {
 
-  # Run the following command to determine which package owns the file:
-  exec { 'rpm_qf':
-    command => "rpm -qf ${rpm_filename}",
-    return  => $rpm_package
-  }
+    String harden_linux::rpm_package = ''
 
-  if ($rpm_package != '') {
-
-    # Set file permissions per rpm package specs...
-    exec { 'rpm_set_perms':
-      command => "rpm --setperms ${rpm_package}"
+    # Run the following command to determine which package owns the file:
+    exec { 'v71849_0':
+      command => "rpm -qf ${rpm_filename}",
+      return  => $harden_linux::rpm_package,
     }
 
-    # Set file owner per rpm package specs...
-    exec { 'rpm_set_owner':
-      command => "rpm --setugids ${rpm_package}"
+    # Set file permissions and owner per rpm package spec...
+    if ($harden_linux::rpm_package != '') {
+
+      exec { 'v71849_1':
+        command => "rpm --setperms ${harden_linux::rpm_package}",
+      }
+
+      exec { 'v71849_2':
+        command => "rpm --setugids ${harden_linux::rpm_package}",
+      }
+
     }
 
   }
