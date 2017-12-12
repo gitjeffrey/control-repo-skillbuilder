fa22a721ccbedd75478c5ec4f879d3f8a5456c85
Jeffrey Frelick <jeff@autostructure.io>
2017-12-12 12:40:13 -0500

    Expanding facts.

--- modules/harden_linux/lib/facter/rpm_system_files.rb
+++ modules/harden_linux/lib/facter/rpm_system_files.rb
@@ -3,8 +3,21 @@
 # To see a custom fact, run "puppet facts" or "facter -p rpm_system_files"
 
 Facter.add('rpm_system_files') do
+
   confine :kernel => 'Linux'
+
   setcode do
-    Facter::Core::Execution.exec("rpm -Va | grep \'^.M\' | grep -o \'/.*$\'").split("\n")
+
+    rpm_hash = {}
+    rpm_array = []
+    rpm_array = Facter::Core::Execution.exec("rpm -Va | grep \'^.M\' | grep -o \'/.*$\'").split("\n")
+
+    rpm_array.each do |rpm_filename|
+      rpm_hash[rpm_filename] = Facter::Core::Execution.exec("rpm -qf #{rpm_filename}")
+    end
+
+    rpm_hash
+
   end
+
 end
