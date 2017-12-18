c13e5852733d0760ceb5430a59342ff1a4adf4c4
Jeffrey Frelick <jeff@autostructure.io>
2017-12-18 09:55:32 -0500

    fixing banner msg requirements

--- modules/harden_linux/manifests/secure_system.pp
+++ modules/harden_linux/manifests/secure_system.pp
@@ -35,22 +35,15 @@ class harden_linux::secure_system {
 
 
   # V-71859 | SRG-OS-000023-GPOS-00006
+  # V-71861 | SRG-OS-000023-GPOS-00006
   # Source: https://help.gnome.org/admin/system-admin-guide/stable/login-banner.html.en
 
   $short_banner_msg = "I've read & consent to terms in IS user agreem't."
 
-  $banner_msg = "You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only.
-By using this IS (which includes any device attached to this IS), you consent to the following conditions:
--The USG routinely intercepts and monitors communications on this IS for purposes including, but not limited to, penetration testing, COMSEC monitoring, network operations and defense, personnel misconduct (PM), law enforcement (LE), and counterintelligence (CI) investigations.
--At any time, the USG may inspect and seize data stored on this IS.
--Communications using, or data stored on, this IS are not private, are subject to routine monitoring, interception, and search, and may be disclosed or used for any USG-authorized purpose.
--This IS includes security measures (e.g., authentication and access controls) to protect USG interests--not for your personal benefit or privacy.
-zx-Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants. Such communications and work product are private and confidential. See User Agreement for details."
-
-  notice("DoD STIG: Vulnerability V-71859 starting.  Details: gnome_version_file_exists=${facts['gnome_version_file_exists']}")
+  $banner_msg = "You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only.\nBy using this IS (which includes any device attached to this IS), you consent to the following conditions:\n-The USG routinely intercepts and monitors communications on this IS for purposes including, but not limited to, penetration testing, COMSEC monitoring, network operations and defense, personnel misconduct (PM), law enforcement (LE), and counterintelligence (CI) investigations.\n-At any time, the USG may inspect and seize data stored on this IS.\n-Communications using, or data stored on, this IS are not private, are subject to routine monitoring, interception, and search, and may be disclosed or used for any USG-authorized purpose.\n-This IS includes security measures (e.g., authentication and access controls) to protect USG interests--not for your personal benefit or privacy.\n-Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants. Such communications and work product are private and confidential. See User Agreement for details."
 
   if ($facts['gnome_version_file_exists']) {
-    notice("DoD STIG: Vulnerability V-71859 passed if().  Details: gnome_version_file_exists=${facts['gnome_version_file_exists']}")
+
     file { '/etc/dconf/profile/gdm':
       ensure  => file,
       owner   => 'root',
@@ -66,7 +59,7 @@ file-db:/usr/share/gdm/greeter-dconf-defaults",
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
-      content => "[org/gnome/login-screen]\nbanner-message-enable=true\nbanner-message-text='${short_banner_msg}'",
+      content => "[org/gnome/login-screen]\nbanner-message-enable=true\nbanner-message-text='${banner_msg}'",
     }
 
     # Added this directory location due to GNOME help.
