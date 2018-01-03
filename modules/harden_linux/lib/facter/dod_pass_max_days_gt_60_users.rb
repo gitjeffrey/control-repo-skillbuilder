0739ae5c55a5bfb01eea9b3102d73a4d8a36d506
Jeffrey Frelick <jeff@autostructure.io>
2018-01-03 16:38:58 -0500

    fixing new fact for V-71933

--- modules/harden_linux/lib/facter/dod_pass_max_days_gt_60_users.rb
+++ modules/harden_linux/lib/facter/dod_pass_max_days_gt_60_users.rb
@@ -2,6 +2,32 @@
 # V-71931
 # To see a custom fact, run "puppet facts" or "facter -p <fact_name>"
 
+# NOTES:
+# 
+# '!' and '!!' mean essentially the same thing,
+# but different tools use one or the other,
+# passwd -l for instance, uses a pair of exclamation points.
+# usermod -L on the other hand only uses one.
+#
+# Usually, accounts with '*' never had a password
+# (as in, have always been disabled for login).
+# This is different to an account with no password hash entry at all,
+# in which case no password is needed (and often won't even be prompted for)
+# which is nearly always BAD!.
+#
+# If it's an invalid hash (which all of '*', '!', and '!!' are) it effectively
+# locks the account and prevents logins to that account.
+# Often this is furthered by setting the account's shell to something like
+# /bin/false or /sbin/nologin in the /etc/passwd file
+#
+# You'll often find that if a user's account is locked after previously having
+# a valid password set, that password hash has exclamation marks prefixed to it,
+# this is so when the account is unlocked the password resumes working again.
+#
+# Source:
+# https://superuser.com/questions/623881/what-means-and-at-second-field-of-etc-shadow
+
+
 Facter.add('dod_pass_max_days_gt_60_users') do
   confine :kernel => 'Linux'
   user_array = []
---
+++ modules/harden_linux/lib/facter/dod_pass_reuse_generations.rb
@@ -0,0 +1,17 @@
+# dod_pass_reuse_generations.rb
+# V-71933
+# To see a custom fact, run "puppet facts" or "facter -p <fact_name>"
+
+# password sufficient pam_unix.so use_authtok sha512 shadow remember=5
+
+Facter.add('dod_pass_reuse_generations') do
+  confine :kernel => 'Linux'
+  retval = Facter::Core::Execution.exec("egrep '^password\s+requisite\s+pam_pwquality.so' /etc/pam.d/system-auth-ac | egrep -o 'retry[\s]*=[\s]*.' | awk -F= '{print $2}'").to_i
+  setcode do
+    if retval!=nil
+      retval
+    else
+      "missing"
+    end
+  end
+end
