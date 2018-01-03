f2aa22a3fdd2cff08ace10c1176f5beae3d61e02
Jeffrey Frelick <jeff@autostructure.io>
2018-01-03 11:38:54 -0500

    puts debug statements.

--- modules/harden_linux/lib/facter/dod_userlist_min_pwd_lifetime_violated.rb
+++ modules/harden_linux/lib/facter/dod_userlist_min_pwd_lifetime_violated.rb
@@ -7,7 +7,7 @@ Facter.add('dod_userlist_min_pwd_lifetime') do
   setcode do
     user_array = []
     user_array = Facter::Core::Execution.exec("awk -F: '{if ($4<1 && $2!=\"*\" && $2!=\"!!\" && $2!=\"!\") print $1}' /etc/shadow").to_s.strip.split("\n")
-    puts user_array.to_s
+    puts "user_array.to_s=" + user_array.to_s
     #if !user_array.empty?
     #  user_array = []
     #else
