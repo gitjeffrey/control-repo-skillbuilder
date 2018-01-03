5b97469fce663f39a1e0a3c59646881a4355a1de
Jeffrey Frelick <jeff@autostructure.io>
2018-01-03 14:18:03 -0500

    fix fact

--- modules/harden_linux/lib/facter/dod_pass_min_days_lt_1_users.rb
+++ modules/harden_linux/lib/facter/dod_pass_min_days_lt_1_users.rb
@@ -1,8 +1,8 @@
-# dod_userlist_min_pwd_lifetime.rb
+# dod_pass_min_days_lt_1_users.rb
 # V-71927
 # To see a custom fact, run "puppet facts" or "facter -p <fact_name>"
 
-Facter.add('dod_userlist_min_pwd_lifetime') do
+Facter.add('dod_pass_min_days_lt_1_users') do
   confine :kernel => 'Linux'
   user_array = []
   user_array = Facter::Core::Execution.exec("awk -F: '{if ($4<1 && $2!=\"*\" && $2!=\"!!\" && $2!=\"!\") print $1}' /etc/shadow").to_s.strip.split("\n")
