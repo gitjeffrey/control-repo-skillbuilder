c1d8a483bcc696e734a255f2d30997ed85cf7c02
Jeffrey Frelick <jeff@autostructure.io>
2018-01-03 10:22:55 -0500

    fixing empty hashes/arrays.

--- modules/harden_linux/lib/facter/dod_userlist_min_pwd_lifetime_violated.rb
+++ modules/harden_linux/lib/facter/dod_userlist_min_pwd_lifetime_violated.rb
@@ -5,9 +5,13 @@
 Facter.add('dod_userlist_min_pwd_lifetime') do
   confine :kernel => 'Linux'
   setcode do
-    user_array = []
-    # awk -F: '$4 < 1 {print $1}' /etc/shadow
-    user_array = Facter::Core::Execution.exec("awk -F: '{if ($4<1 && $2!=\"*\" && $2!=\"!!\" && $2!=\"!\") print $1}' /etc/shadow").strip.split("\n")
+    #user_array = []
+    user_string = Facter::Core::Execution.exec("awk -F: '{if ($4<1 && $2!=\"*\" && $2!=\"!!\" && $2!=\"!\") print $1}' /etc/shadow").strip
+    if (user_string.empty?)
+      user_array = []
+    else
+      user_array = user_string.split("\n")
+    end
     user_array
   end
 end
